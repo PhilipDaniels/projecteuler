b31e2421cb828b9b906f6f0ce7bdc9c616230ab9
Philip Daniels <Philip.Daniels1971@gmail.com>
2018-03-16 22:31:00 +0000

    Solve problem 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn p001() {
+    let answer = (0..1000).filter(|&x| x % 3 == 0 || x % 5 == 0).sum::<usize>();
+    println!("p001 answer = {}", answer);
+}
+
+fn p002() {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    p001();
+    p002();
 }
