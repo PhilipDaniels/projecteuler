2f38548908b5a02b27cbee75242a6356679e8416
Philip Daniels <Philip.Daniels1971@gmail.com>
2018-03-17 11:50:56 +0000

    Start problem 3. Add PrimeIterator.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod fibonacci;
+mod prime;
 
 use fibonacci::FibonacciIterator;
+use prime::PrimeIterator;
 
 fn p001() {
     let answer = (0..1000)
@@ -14,13 +16,24 @@ fn p001() {
 fn p002() {
     let answer = FibonacciIterator::new()
         .filter(|&x| x % 2 == 0)
-        .take_while(|&x| x < 4000 * 1000)
+        .take_while(|&x| x < 4_000_000)
         .sum::<usize>();
 
     println!("p002 answer = {}", answer);
 }
 
+fn p003() {
+    let p = PrimeIterator::new().skip(100000).next().unwrap();
+    println!("prime(10000) = {}", p);
+
+//    for (idx, p) in PrimeIterator::new().take(25).enumerate() {
+//        //println!("prime({}) = {}", idx, p);
+//
+//    }
+}
+
 fn main() {
     p001();
     p002();
+    p003();
 }
---
+++ src/prime.rs
@@ -0,0 +1,54 @@
+/// An iterator that yields prime numbers.
+/// The values start: 2, 3, 5, 7, 11, 13, 17, 19, ...
+pub struct PrimeIterator {
+    primes: Vec<u64>,
+    prev: u64
+}
+
+impl PrimeIterator {
+    pub fn new() -> Self {
+        PrimeIterator { primes: Vec::new(), prev: 0 }
+    }
+}
+
+impl Iterator for PrimeIterator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        // Special cases so we can optimise with +2 rather than +1 in the loop below.
+        if self.prev == 0 {
+            self.primes.push(2);
+            self.prev = 2;
+            return Some(self.prev);
+        } else if self.prev == 2 {
+            self.primes.push(3);
+            self.prev = 3;
+            return Some(self.prev);
+        }
+
+        self.prev += 2;
+        while !is_prime(&self.primes, self.prev) {
+            self.prev += 2;
+        }
+
+        self.primes.push(self.prev);
+        return Some(self.prev);
+    }
+}
+
+fn is_prime(known_primes: &Vec<u64>, n: u64) -> bool {
+    let upper_limit = (n as f64).sqrt() as u64 + 1;
+    //println!("   n = {}, upper_limit = {}", n, upper_limit);
+
+    for &p in known_primes {
+        if p >= upper_limit {
+            return true;
+        }
+        if n % p == 0 {
+            return false;
+        }
+    }
+
+    true
+}
+
